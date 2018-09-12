c1f99585b948a66e499bd3d086261133d530b540
Advent21 <Advent21@users.noreply.github.com>
2018-09-12 15:08:10 -0400

    Data.cpp

---
+++ Data.cpp
@@ -0,0 +1,42 @@
+#include "Data.h"
+
+	Data::Data()
+    {
+	x = 0 ;
+	y = 0 ;
+    }
+
+	Data::Data(int a , float b)
+    {
+	x = a;
+	y = b;
+    }
+
+	int Data::getx()
+    {
+	return x;
+    }
+
+	float Data::gety()
+    {
+	return y;
+    }
+
+	void Data::setx(int a)
+    {
+	x = a;
+    }
+
+
+	void Data::sety(float a)
+    {
+
+	y = a;
+    }
+
+
+
+
+
+
+
