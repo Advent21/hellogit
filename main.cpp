55f8db8ea29636d515623568c6f25f9c83416f0f
Advent21 <Advent21@users.noreply.github.com>
2018-10-03 15:03:59 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,31 +6,22 @@ using namespace std;
 	int main
 {
 
-   	vector<Data>v1;
-	vector<Data>v2= {5,8,22};
-
-	v1.pushback(12);
-	v1.pushback(17);
-	v2.popback();
-
-    cout<<"v2 has"<<v2.size()<<"elements";
-
-    cout<<"v1 has"<<v1.size()<<"elements";
-
-	Data d0;
-	Data d1 = {7,24.83};
+   	vector<Data>v;
 	
-	d0.setx(15);
-	d1.sety(3.14);
+	v.push_back({7,21.01});
 
+	Data d;
 	
-	for( auto a:v)
-  {
-	cout<<a.getx()<<" ";	
+	
+	d.setx(10);
+	d.sety(12.10);
+	v.push_back(d);
+
+
+	for(auto a:v)
+   {
+	cout<<a.getx()<<" ";
 	cout<<a.gety()<<endl;
-  }
-
-	
 }
 
 
