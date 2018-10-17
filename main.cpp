02620a705b9035232867da68dc2fc05c97af4c62
Advent21 <43126138+Advent21@users.noreply.github.com>
2018-10-17 13:39:01 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,24 @@ using namespace std;
 	cout<<a.getx()<<" ";
 	cout<<a.gety()<<endl;
    }
+		
+	void bubblesort(auto& Data)
+	{
+	cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
 
+	for ( int i = 0 ; i < Data.size() ; i++)
+			{
+		for ( int j = 0 ; j < Data.size()-1 ; j++)
+			if ( Data [ j+1] < Data [ j ] )
+			swap ( Data [ j +1] , Data [ j ] ) ;
+
+	print(Data);
+			}
+	}
+		bubblesort(v);
+		for(auto i : v)
+			cout<<i<<" ";
+			cout<<endl;
 }
 
 
